2ca8f8586eca485add7d880f240599edb3ba5060
Gaxi2 <103119685+Gaxi2@users.noreply.github.com>
2022-06-06 11:32:40 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,34 +19,33 @@ class Config(object):
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
-This is Permanent Files Store Bot!
-Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
+Hello👋,
+This is Finder004 here. I am here to provide you the requested files from Find_Hub.
 
-🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
 
-📝 **Language:** [Python3](https://www.python.org)
 
-📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
 
-📡 **Hosted on:** [Heroku](https://heroku.com)
 
-🧑🏻‍💻 **Developer:** @AbirHasan2005
 
-👥 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
 
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+
+
+
+
+👥 **Support Group:** [Click Here](https://t.me/findhubsupport)
+
+📢 **Updates Channel:** [Click Here](https://t.me/findhubupdates)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+As I said, I am Finder004.
+There is nothing to know about me.
+If you know me, you know me 👀.
 
-Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
-Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
 """
 	HOME_TEXT = """
-Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
+Hey👋, [{}](tg://user?id={})\n\nThis is finder here.
 
-Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
+I am here to provide you the requested files from Find_Hub.
 """
